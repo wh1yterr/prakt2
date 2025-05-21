e977161ba1a2a773e0adc5af45397fd7c34b0fac
wh1yte <wh1ytettv@gmail.com>
2025-05-21 20:12:31 +0300

    register changes

--- back/app.py
+++ back/app.py
@@ -15,16 +15,25 @@ with app.app_context():
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
-    required_fields = ['org', 'fio', 'phone', 'email']
 
+    # Проверка на заполненность
+    required_fields = ['company_name', 'contact_name', 'email', 'phone']
     if not data or not all(field in data and data[field].strip() for field in required_fields):
         return jsonify({'error': 'Пожалуйста, заполните все обязательные поля.'}), 400
 
+    # Сохранение в БД
+    reg = Registration(
+        company_name=data['company_name'],
+        contact_name=data['contact_name'],
+        email=data['email'],
+        phone=data['phone'],
+        approved=False
+    )
+    db.session.add(reg)
+    db.session.commit()
 
-    pending_registrations.append(data)
+    return jsonify({'message': 'Заявка успешно сохранена!'}), 200
 
-
-    return jsonify({'message': 'Заявка успешно отправлена!'}), 200
 
 @app.route('/all')
 def all_regs():
